78854c1938f17a7214d9e0a9dba28dac8d276b41
maheshmakude <mahesh.makude@gmail.com>
2012-06-27 13:22:57 +0530

    Update master

--- TestFizzBuzzMocked.py
+++ TestFizzBuzzMocked.py
@@ -6,8 +6,9 @@ Q5. Write the psuedocode for the test_repport method, such that it uses PyMock
     mock objects to test the report method of FizzBuzz. [5 pts]
 """
 class TestFizzBuzzMocked(pymock.PyMockTestCase):
-        
+
     def setUp(self):
+        
         super(TestFizzBuzzMocked, self).setUp()
         self.fb = FizzBuzz.FizzBuzz()
         print "setUp TestFizzBuzzMocked"
@@ -17,23 +18,22 @@ class TestFizzBuzzMocked(pymock.PyMockTestCase):
         self.fb = None
 
     def test_report(self):
-        pass
+
+        #create mock
+        mockFileProvider = self.mock()
+        mockFileWrapper = self.mockFileProvider('report.txt', 'w')
+
+        #replay
+        self.replay()
+
+        #Call the report method
+        numbers=range(100)
+        FizzBuzz.report(self.numbers,self.mockFileWrapper)   
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-        
+   
 
 
 if __name__ == "__main__":
